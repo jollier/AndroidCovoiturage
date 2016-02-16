248bd08b695a3219b7f316dfc945ef13a70d2d69
julien.ollier <julien.ollier@berger-levrault.fr>
2016-02-16 23:04:36 +0100

    verification connexion sur serveur + début d'un TU mais probleme avec Thread

--- app/src/androidTest/java/com/fousduvolant/androidcovoiturage/ApplicationTest.java
+++ app/src/androidTest/java/com/fousduvolant/androidcovoiturage/ApplicationTest.java
@@ -2,12 +2,23 @@ package com.fousduvolant.androidcovoiturage;
 
         import android.app.Application;
         import android.test.ApplicationTestCase;
+        import android.test.suitebuilder.TestSuiteBuilder;
+
+        import com.fousduvolant.androidcovoiturage.FousDuVolant;
+
+        import junit.framework.Test;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest extends JunitTestCase<FousDuVolant> {
+    public static Test suite() {
+        return new TestSuiteBuilder(ApplicationTest.class).includeAllPackagesUnderHere().build();
+    }
     public ApplicationTest() {
-        super(Application.class);
+        super(FousDuVolant.class);
     }
 }
