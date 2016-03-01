cc4a34be9bc64e350442cd4c72980848bf982cdb
julien.ollier <julien.ollier@berger-levrault.fr>
2016-03-01 14:03:28 +0100

    1er test unitaire pas encore fini

--- app/src/androidTest/java/com/fousduvolant/androidcovoiturage/ApplicationTest.java
+++ app/src/androidTest/java/com/fousduvolant/androidcovoiturage/ApplicationTest.java
@@ -1,26 +1,11 @@
 package com.fousduvolant.androidcovoiturage;
 
-        import android.app.Application;
-        import android.test.ApplicationTestCase;
-        import android.test.suitebuilder.TestSuiteBuilder;
-
-        import com.fousduvolant.androidcovoiturage.FousDuVolant;
-
-        import junit.framework.Test;
-
-        import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 
 
+public class ApplicationTest {
 
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public static Test suite() {
-        return new TestSuiteBuilder(ApplicationTest.class).includeAllPackagesUnderHere().build();
-    }
-    public ApplicationTest() {
-        super(Application.class);
-    }
 }
