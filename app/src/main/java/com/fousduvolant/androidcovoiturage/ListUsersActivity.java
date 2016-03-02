f4f73575bb81b0a2150fe2da179f227a3cec0988
FrodonSaquet <alexandre.redon@berger-levrault.fr>
2016-03-02 09:11:15 +0100

    ListUsersActivity

--- app/src/main/java/com/fousduvolant/androidcovoiturage/ListUsersActivity.java
+++ app/src/main/java/com/fousduvolant/androidcovoiturage/ListUsersActivity.java
@@ -5,8 +5,20 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,15 +44,19 @@ import service.InputStreamOperations;
 /**
  * Created by AREDON on 17/02/2016.
  */
-public class ListUsersActivity extends Activity{
+public class ListUsersActivity extends AppCompatActivity implements OnMapReadyCallback{
     ListView myListView;
+    private GoogleMap mMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_users_activity_layout_listview_main);
-
         Intent intent = getIntent();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         myListView = (ListView) findViewById(R.id.listView);
 
@@ -78,6 +94,16 @@ public class ListUsersActivity extends Activity{
 
         // 3. setListAdapter
         listView.setAdapter(adapter);
+
+        // 4. setListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String message = "Clicked entry = " + position;
+                Log.i("ListView", message);
+            }
+        });
 
     }
 
@@ -208,4 +234,34 @@ public class ListUsersActivity extends Activity{
         }
 
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng BL = new LatLng(43.5432556,1.5100196);
+        mMap.addMarker(new MarkerOptions()
+                .position(BL)
+                .title("Berger-Levrault"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BL));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder() .
+                target(BL).tilt(45).zoom(13).bearing(0).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+    }
+
+
 }
